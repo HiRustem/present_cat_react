e34649abc0dd2c122474efdec40ed1f32b62150b
Rustem <rustem_hajrullin@enot.work>
2025-02-17 09:20:10 +0300

    fix: move box in box scene

--- src/components/BoxScene/BoxScene.module.scss.d.ts
+++ src/components/BoxScene/BoxScene.module.scss.d.ts
@@ -1,4 +1,15 @@
 declare const classNames: {
   readonly wrapper: "wrapper";
+  readonly scene: "scene";
+  readonly box: "box";
+  readonly content: "content";
+  readonly front: "front";
+  readonly back: "back";
+  readonly left: "left";
+  readonly right: "right";
+  readonly bottom: "bottom";
+  readonly appearAnimation: "appearAnimation";
+  readonly animationWrapper: "animationWrapper";
+  readonly mainAnimation: "mainAnimation";
 };
 export = classNames;
